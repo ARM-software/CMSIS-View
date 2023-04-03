0bd48c37c558466986a1f1f796841a064a47da1b
Milorad Cvjetkovic <milorad.cvjetkovic@arm.com>
2023-04-03 09:01:43 +0200

    Update copyright

--- Fault/Include/ARM_Fault.h
+++ Fault/Include/ARM_Fault.h
@@ -1,11 +1,20 @@
-/*------------------------------------------------------------------------------
- * MDK - Component ARM::CMSIS-View:Fault
- * Copyright (c) 2022-2023 ARM Germany GmbH. All rights reserved.
- *------------------------------------------------------------------------------
- * Name:    ARM_Fault.h
- * Purpose: ARM Fault Header File
- * Rev.:    V0.4.0
- *----------------------------------------------------------------------------*/
+/*
+ * Copyright (c) 2022-2023 Arm Limited. All rights reserved.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Licensed under the Apache License, Version 2.0 (the License); you may
+ * not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an AS IS BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 #ifndef ARM_FAULT_H__
 #define ARM_FAULT_H__
